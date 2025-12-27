0ca8642c4c42b033352e84cca878f6163de0f048
ali-hamad0 <alimufidhamad0000@gmail.com>
2025-12-27 15:13:12 +0200

    s

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use("/uploads", express.static(uploadsDir));
 // ===============================
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT || 3306),
+  port: Number(process.env.DB_PORT || 330),
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "NewPass123!",
   database: process.env.DB_NAME || "dr_online_db",
 });
 
